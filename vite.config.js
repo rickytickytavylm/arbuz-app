19145bb7fd85eca2a5a75c6550d3412ef70556ec
rickytickytavy <ricksanchezlm7@gmail.com>
2025-02-18 18:42:43 +0300

    Исправил пути для GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/watermelon-app/",
+  base: "/arbuz-app/",  // Укажи имя своего репозитория на GitHub
   plugins: [react()],
 });
-
-
